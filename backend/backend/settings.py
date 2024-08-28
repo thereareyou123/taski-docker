289fafde49deaaf09b152cdb4afc1d20c25a0a47
Никита Рохлин <thereareyou@yahoo.com>
2024-08-28 13:58:42 +0300

    ready

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://sanyabankai.myftp.biz']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sanyabankai.myftp.biz']
 
 
 # Application definition
